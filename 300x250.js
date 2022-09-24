712b9811ae7551a7398a80489bdc66003308a6ea
shurikienimake <114287414+shurikienimake@users.noreply.github.com>
2022-09-24 17:10:13 +0700

    Create 300x250.js

---
+++ 300x250.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '20c7890069d8e41380a2be72f70c6f11',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/20c7890069d8e41380a2be72f70c6f11/invoke.js"></scr' + 'ipt>');
